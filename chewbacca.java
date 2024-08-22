1b921e89ca9ba04b4c71776b6da8e51b2f44bc29
helper.dev <basakayush942@gmail.com>
2024-08-22 14:27:41 +0530

    Update chewbacca.java

--- chewbacca.java
+++ chewbacca.java
@@ -5,17 +5,17 @@ public class chewbacca {
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     String number=sc.next();// read the number as string
-    StringBuilder req=new StringBuilder();// strinf is not mutable it's mutable in which ans will be strored
-    for (int i = 0; i < number.length(); i++) {
-      int n=number.charAt(i)-'0';//number-string=number
+    StringBuilder req=new StringBuilder();// string is not mutable it's mutable in which ans will be strored
+    for (int i = 0; i < number.length(); i++) {// string is fixed but stringbulder is not 
+      int n=number.charAt(i)-'0';//number-string=number ==> conterving string to number
       int n_2=(9-n);
       if (i==0 && n==9) {//==> 1st digit
         // is 1st digit (9) become 0 make no sense
-        req.append(n);
+        req.append(n);// append fxn ==> in string bulder it's used to add new string
         }
         else{
-          req.append(Math.min(n, n_2));
-        }
+          req.append(Math.min(n, n_2));//b/w  t and (9-t) which is is smaller append that
+        }// append meas adding it 
      
       //the number of time we want  to scan and change the numnet and exchnage it with (9-t)
       // obersertion in prblm 2
@@ -30,11 +30,9 @@ public class chewbacca {
        * so min value b/w 4 and 5 will not be replaced
        * and 4th one will remain the same 
        * edge cases will be seen while coding 
-       * to analyse earch and every number 1 st to last the int should be converted to string that's why we will use stringbuilder form
-       */
-      
-    }
-    System.out.println(req.toString());
-    sc.close();
-  }// prblm link ;-> https://codeforces.com/contest/514/problem/A
+       * to analyse earch and every number 1 st to last the int should be converted to string that's why we will use stringbuilder form     */
 }
+    System.out.println(req.toString());//we giving it as string cz go trough of all number is not possible that's why we have to use it 
+    sc.close();
+  }
+}// prblm link :-> https://codeforces.com/contest/514/problem/A
